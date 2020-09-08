4602eaac08cca40151e1a739ce96ffad255adfdb
ShobhitRatan <shobhit.ratan@baruchmail.cuny.edu>
2020-09-08 19:51:29 -0400

    Updates

---
+++ precache-manifest.b1ae7c0f0ab279e9c7a037e196eecc88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29af11087af7dd97e4fd648802330985",
+    "url": "/chefsworld-frontend/index.html"
+  },
+  {
+    "revision": "2607eef82b5298c77a3e",
+    "url": "/chefsworld-frontend/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7418c066c12aa327f0b7",
+    "url": "/chefsworld-frontend/static/css/main.d069818e.chunk.css"
+  },
+  {
+    "revision": "2607eef82b5298c77a3e",
+    "url": "/chefsworld-frontend/static/js/2.6af83bc2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/chefsworld-frontend/static/js/2.6af83bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7418c066c12aa327f0b7",
+    "url": "/chefsworld-frontend/static/js/main.4600255c.chunk.js"
+  },
+  {
+    "revision": "26fc7bd4da2e0b171d1b",
+    "url": "/chefsworld-frontend/static/js/runtime-main.8b637755.js"
+  }
+]);
